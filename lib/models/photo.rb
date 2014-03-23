82be9ad72ea4ab93519f4169215641eb3030bd85
Jarrod Taylor <jarrodtaylor@icloud.com>
2014-03-23 12:41:03 -0400

    Adds carrierwave

--- lib/models/photo.rb
+++ lib/models/photo.rb
@@ -1,7 +1,22 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+                 provider: ENV.fetch('PROVIDER')
+  }
+  config.fog_directory  = ENV.fetch('FOG_DIRECTORY')
+end
+
+class Uploader < CarrierWave::Uploader::Base
+  storage :fog
+end
+
 class Photo < Sequel::Model
   plugin :timestamps
 
   many_to_one :product
   many_to_one :fader
   many_to_one :logo
+
+  mount_uploader :source, Uploader
 end

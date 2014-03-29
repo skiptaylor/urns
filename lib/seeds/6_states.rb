28b125499990df233e46f2ef91025fd1d091bd5c
Skip Taylor <skiptaylor@clear.net>
2014-03-29 08:10:11 -0400

    fixes states and zones seeds

--- lib/seeds/6_states.rb
+++ lib/seeds/6_states.rb
@@ -1,55 +1,55 @@
-one   = Region.create region: 'one'
-two   = Region.create region: 'two'
-three = Region.create region: 'three'
-four  = Region.create region: 'four'
+one   = Zone.create zone: 'one'
+two   = Zone.create zone: 'two'
+three = Zone.create zone: 'three'
+four  = Zone.create zone: 'four'
 
-State.create name: 'Alabama',        abbr: 'AL', region_id: one.id
-State.create name: 'Alaska',         abbr: 'AK', region_id: four.id
-State.create name: 'Arizona',        abbr: 'AZ', region_id: three.id
-State.create name: 'Arkansas',       abbr: 'AR', region_id: one.id
-State.create name: 'California',     abbr: 'CA', region_id: three.id
-State.create name: 'Colorado',       abbr: 'CO', region_id: two.id
-State.create name: 'Connecticut',    abbr: 'CT', region_id: two.id
-State.create name: 'Delaware',       abbr: 'DE', region_id: one.id
-State.create name: 'Florida',        abbr: 'FL', region_id: one.id
-State.create name: 'Georgia',        abbr: 'GA', region_id: one.id
-State.create name: 'Hawaii',         abbr: 'HI', region_id: four.id
-State.create name: 'Idaho',          abbr: 'ID', region_id: three.id
-State.create name: 'Illinois',       abbr: 'IL', region_id: one.id
-State.create name: 'Indiana',        abbr: 'IN', region_id: one.id
-State.create name: 'Iowa',           abbr: 'IA', region_id: two.id
-State.create name: 'Kansas',         abbr: 'KS', region_id: two.id
-State.create name: 'Kentucky',       abbr: 'KY', region_id: one.id
-State.create name: 'Louisiana',      abbr: 'LA', region_id: one.id
-State.create name: 'Maine',          abbr: 'ME', region_id: two.id
-State.create name: 'Maryland',       abbr: 'MD', region_id: one.id
-State.create name: 'Massachusetts',  abbr: 'MA', region_id: two.id
-State.create name: 'Michigan',       abbr: 'MI', region_id: one.id
-State.create name: 'Minnesota',      abbr: 'MN', region_id: two.id
-State.create name: 'Mississippi',    abbr: 'MS', region_id: one.id
-State.create name: 'Missouri',       abbr: 'MO', region_id: one.id
-State.create name: 'Montana',        abbr: 'MT', region_id: three.id
-State.create name: 'Nebraska',       abbr: 'NE', region_id: two.id
-State.create name: 'Nevada',         abbr: 'NV', region_id: three.id
-State.create name: 'New Hampshire',  abbr: 'NH', region_id: two.id
-State.create name: 'New Jersey',     abbr: 'NJ', region_id: one.id
-State.create name: 'New Mexico',     abbr: 'NM', region_id: two.id
-State.create name: 'New York',       abbr: 'NY', region_id: two.id
-State.create name: 'North Carolina', abbr: 'NC', region_id: one.id
-State.create name: 'North Dakota',   abbr: 'ND', region_id: one.id
-State.create name: 'Ohio',           abbr: 'OH', region_id: one.id
-State.create name: 'Oklahoma',       abbr: 'OK', region_id: two.id
-State.create name: 'Oregon',         abbr: 'OR', region_id: three.id
-State.create name: 'Pennsylvania',   abbr: 'PA', region_id: one.id
-State.create name: 'Rhode Island',   abbr: 'RI', region_id: two.id
-State.create name: 'South Carolina', abbr: 'SC', region_id: one.id
-State.create name: 'South Dakota',   abbr: 'SD', region_id: two.id
-State.create name: 'Tennessee',      abbr: 'TN', region_id: one.id
-State.create name: 'Texas',          abbr: 'TX', region_id: two.id
-State.create name: 'Utah',           abbr: 'UT', region_id: three.id
-State.create name: 'Vermont',        abbr: 'VT', region_id: two.id
-State.create name: 'Virginia',       abbr: 'VA', region_id: one.id
-State.create name: 'Washington',     abbr: 'WA', region_id: three.id
-State.create name: 'West Virginia',  abbr: 'WV', region_id: one.id
-State.create name: 'Wisconsin',      abbr: 'WI', region_id: two.id
-State.create name: 'Wyoming',        abbr: 'WY', region_id: three.id
+State.create name: 'Alabama',        abbr: 'AL', zone_id: one.id
+State.create name: 'Alaska',         abbr: 'AK', zone_id: four.id
+State.create name: 'Arizona',        abbr: 'AZ', zone_id: three.id
+State.create name: 'Arkansas',       abbr: 'AR', zone_id: one.id
+State.create name: 'California',     abbr: 'CA', zone_id: three.id
+State.create name: 'Colorado',       abbr: 'CO', zone_id: two.id
+State.create name: 'Connecticut',    abbr: 'CT', zone_id: two.id
+State.create name: 'Delaware',       abbr: 'DE', zone_id: one.id
+State.create name: 'Florida',        abbr: 'FL', zone_id: one.id
+State.create name: 'Georgia',        abbr: 'GA', zone_id: one.id
+State.create name: 'Hawaii',         abbr: 'HI', zone_id: four.id
+State.create name: 'Idaho',          abbr: 'ID', zone_id: three.id
+State.create name: 'Illinois',       abbr: 'IL', zone_id: one.id
+State.create name: 'Indiana',        abbr: 'IN', zone_id: one.id
+State.create name: 'Iowa',           abbr: 'IA', zone_id: two.id
+State.create name: 'Kansas',         abbr: 'KS', zone_id: two.id
+State.create name: 'Kentucky',       abbr: 'KY', zone_id: one.id
+State.create name: 'Louisiana',      abbr: 'LA', zone_id: one.id
+State.create name: 'Maine',          abbr: 'ME', zone_id: two.id
+State.create name: 'Maryland',       abbr: 'MD', zone_id: one.id
+State.create name: 'Massachusetts',  abbr: 'MA', zone_id: two.id
+State.create name: 'Michigan',       abbr: 'MI', zone_id: one.id
+State.create name: 'Minnesota',      abbr: 'MN', zone_id: two.id
+State.create name: 'Mississippi',    abbr: 'MS', zone_id: one.id
+State.create name: 'Missouri',       abbr: 'MO', zone_id: one.id
+State.create name: 'Montana',        abbr: 'MT', zone_id: three.id
+State.create name: 'Nebraska',       abbr: 'NE', zone_id: two.id
+State.create name: 'Nevada',         abbr: 'NV', zone_id: three.id
+State.create name: 'New Hampshire',  abbr: 'NH', zone_id: two.id
+State.create name: 'New Jersey',     abbr: 'NJ', zone_id: one.id
+State.create name: 'New Mexico',     abbr: 'NM', zone_id: two.id
+State.create name: 'New York',       abbr: 'NY', zone_id: two.id
+State.create name: 'North Carolina', abbr: 'NC', zone_id: one.id
+State.create name: 'North Dakota',   abbr: 'ND', zone_id: one.id
+State.create name: 'Ohio',           abbr: 'OH', zone_id: one.id
+State.create name: 'Oklahoma',       abbr: 'OK', zone_id: two.id
+State.create name: 'Oregon',         abbr: 'OR', zone_id: three.id
+State.create name: 'Pennsylvania',   abbr: 'PA', zone_id: one.id
+State.create name: 'Rhode Island',   abbr: 'RI', zone_id: two.id
+State.create name: 'South Carolina', abbr: 'SC', zone_id: one.id
+State.create name: 'South Dakota',   abbr: 'SD', zone_id: two.id
+State.create name: 'Tennessee',      abbr: 'TN', zone_id: one.id
+State.create name: 'Texas',          abbr: 'TX', zone_id: two.id
+State.create name: 'Utah',           abbr: 'UT', zone_id: three.id
+State.create name: 'Vermont',        abbr: 'VT', zone_id: two.id
+State.create name: 'Virginia',       abbr: 'VA', zone_id: one.id
+State.create name: 'Washington',     abbr: 'WA', zone_id: three.id
+State.create name: 'West Virginia',  abbr: 'WV', zone_id: one.id
+State.create name: 'Wisconsin',      abbr: 'WI', zone_id: two.id
+State.create name: 'Wyoming',        abbr: 'WY', zone_id: three.id

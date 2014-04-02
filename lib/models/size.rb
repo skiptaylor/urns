a5a8ee3fa139631b259684efa57ace5c2bbab8ff
Skip Taylor <skiptaylor@clear.net>
2014-04-02 09:06:00 -0400

    removes defs for zone and size

--- lib/models/size.rb
+++ lib/models/size.rb
@@ -1,13 +1,5 @@
 class Size < Sequel::Model
   plugin :timestamps
 
-  def sizename 
-    if self.size_id.nil?
-      "unknown"
-    else
-      self.size.points
-    end
-  end
-  
   one_to_many :costs
 end
--- lib/models/zone.rb
+++ lib/models/zone.rb
@@ -1,13 +1,7 @@
 class Zone < Sequel::Model
   plugin :timestamps
 
-  def zonename 
-    if self.zone_id.nil?
-      "unknown"
-    else
-      self.zone.name
-    end
-  end
+
   
   one_to_many :states
   one_to_many :costs

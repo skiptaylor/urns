09468e144a87566e2161b4a9700afae78ba00ae5
Skip Taylor <skiptaylor@clear.net>
2014-04-02 08:16:23 -0400

    Adds def to zone and size

--- lib/models/zone.rb
+++ lib/models/zone.rb
@@ -1,6 +1,14 @@
 class Zone < Sequel::Model
   plugin :timestamps
 
+  def zonename 
+    if self.zone_id.nil?
+      "unknown"
+    else
+      self.zone.name
+    end
+  end
+  
   one_to_many :states
   one_to_many :costs
   many_to_one :service

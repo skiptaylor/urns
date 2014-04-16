0f0e9a8f3d604caf9a3b90d0d530ce2a8cfeef5a
Skip Taylor <skiptaylor@clear.net>
2014-04-16 15:51:35 -0400

    Adds acorn to engraving

--- lib/models/engraving.rb
+++ lib/models/engraving.rb
@@ -2,4 +2,15 @@ class Engraving < Sequel::Model
   plugin :timestamps
 
   many_to_one :product
+  
+  def acorn_engrave
+    if self.engraving.line1 != ""
+      @product.id_71.price + 15.00
+    elsif self.engraving.line2 != ""
+      @product.id_71.price + 30.00
+    elsif self.engraving.line3 != ""
+      @product.id_71.price + 45.00
+    end
+  end
+  
 end

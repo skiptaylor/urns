13ec5bbec75e52c13e9768b8e96eeb2dc7864a3e
Skip Taylor <skiptaylor@clear.net>
2014-05-25 11:04:20 -0400

    Adds contact migration

---
+++ lib/migrations/201405251055_create_contact.rb
@@ -0,0 +1,20 @@
+Sequel.migration do
+  change do
+    create_table(:contacts) do
+      primary_key :id
+
+      DateTime :created_at
+      DateTime :updated_at
+
+      String  :name
+      String  :company
+      String  :email
+      String  :phone
+      
+      Text    :comment
+      
+      Boolean :reliquary, default: false
+
+    end
+  end
+end
--- lib/routes/contactus.rb
+++ lib/routes/contactus.rb
@@ -1,17 +1,17 @@
 module Urns
   class Routes < Urns::Base
 
-    get "/contactus/contactuses/?" do
-      @contactus = Contactus.all
+    get "/contact/contacts/?" do
+      @contact = Contact.all
       erb :"/admin/dashboard", layout: "/layout_product".to_sym
     end
 
-    get "/contactus/new/?" do
-      @contactus = Contactus.new
+    get "/contact/new/?" do
+      @contact = Contact.new
       erb :"/contactus/contact-us"
     end
 
-    post "/contactus/new/?" do
+    post "/contact/new/?" do
       contactus = Contactus.create(
         :name       => params[:name],
         :company    => params[:company],
@@ -19,34 +19,34 @@ module Urns
         :phone      => params[:phone],
         :comment    => params[:comment]
     )
-      params[:read]    ? contactus.update(read: true)    : contactus.update(read: false)
+      params[:read]    ? contact.update(read: true)    : contact.update(read: false)
 
       redirect "/contactus/thank-you"
     end
 
 
-    get "/contactus/:id/edit/?" do
+    get "/contact/:id/edit/?" do
       @contactus = Contactus[params[:id]]
-      erb :"/contactus/contactus_edit", layout: "/layout_product".to_sym
+      erb :"/contactus/contact_edit", layout: "/layout_product".to_sym
     end
 
-    post "/contactus/:id/edit/?" do
-      contactus = Contactus[params[:id]]
-      contactus.update(
+    post "/contact/:id/edit/?" do
+      contact = Contact[params[:id]]
+      contacs.update(
       :name       => params[:name],
       :company    => params[:company],
       :email      => params[:email],
       :phone      => params[:phone],
       :comment    => params[:comment]
       )
-      params[:read]    ? contactus.update(read: true)    : contactus.update(read: false)
+      params[:read]    ? contact.update(read: true)    : contact.update(read: false)
 
       redirect "/admin/dashboard"
     end
 
-    get "/contactus/:id/delete/?" do
-      contactus = Contactus[params[:id]]
-      contactus.destroy
+    get "/contact/:id/delete/?" do
+      contact = Contact[params[:id]]
+      contact.destroy
       redirect "/admin/dashboard"
     end
   end

53756f011ee732489b1466bce14bb9d3548e0692
Skip Taylor <skiptaylor@clear.net>
2014-06-11 12:29:05 -0400

    fixes sample routes

--- lib/routes/sample.rb
+++ lib/routes/sample.rb
@@ -1,17 +1,12 @@
+class Routes < Urns::Base
 
-  class Routes < Urns::Base
-
-    get '/sample/?' do
-      erb :sample
-    end
-    
-    get '/contact-us/?' do
-      erb :"contact-us"
-    end
-    
-    get '/terms/?' do
-      erb :terms
-    end
-
+  get '/contact-us/?' do
+    erb :"contact-us"
   end
+  
+  get '/terms/?' do
+    erb :terms
+  end
+
+end
 

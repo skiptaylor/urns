9ca25573630cde40a46bf2c6bb6bd7c7b7877410
Skip Taylor <skiptaylor@clear.net>
2014-06-11 09:09:25 -0400

    fixes purchase edit

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -137,7 +137,11 @@ class Routes < Urns::Base
     erb :"/admin/purchase_edit"
   end
 
-  post "/purchase/:id/edit/?" do
+  post "/admin/purchase/:id/edit/?" do
+    cart = ShoppingCartItem.where(shopping_session: session[:shopping_session])
+    total = ShoppingCartItem.total(session[:shopping_session])
+    state = State.all
+    item = Item.all
     purchase = Purchase[params[:id]]
     purchase.update(
       :stripe_id          => params[:stripe_id],

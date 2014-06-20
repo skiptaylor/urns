6cacd43b1e50d2e21d8b7d32e8c0063c07afb30b
Skip Taylor <skiptaylor@clear.net>
2014-06-20 14:21:06 -0400

    Test => true for string ??

--- lib/migrations/201406200130_alter_productstyles.rb
+++ lib/migrations/201406200130_alter_productstyles.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table(:productstyles) do
-      add_column :description_pet, Text, default: ''
+      add_column :description_pet, String, :text => true
     end
   end
 end

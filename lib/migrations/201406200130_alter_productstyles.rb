d9929b864c6a85f76c2b0a7c501ced299b6de613
Skip Taylor <skiptaylor@clear.net>
2014-06-20 14:10:52 -0400

    Adds default

--- lib/migrations/201406200130_alter_productstyles.rb
+++ lib/migrations/201406200130_alter_productstyles.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table(:productstyles) do
-      add_column :description_pet, Text
+      add_column :description_pet, Text, default: ''
     end
   end
 end

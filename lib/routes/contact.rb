a6aeee81b3bb61128c2c4b7a7621dd3e05f0f747
Skip Taylor <skiptaylor@clear.net>
2014-06-21 09:32:22 -0400

    Moved contact route back to gem

---
+++ lib/routes/contact.rb
@@ -0,0 +1,51 @@
+class Routes < Urns::Base
+
+  get "/contact/new/?" do
+    @contact = Contact.new
+    erb :"/contactus/contact-us"
+  end
+
+  post "/contact/new/?" do
+    contact = Contact.create(
+      :name       => params[:name],
+      :company    => params[:company],
+      :email      => params[:email],
+      :phone      => params[:phone],
+      :comment    => params[:comment],
+      :read       => params[:read]
+    )
+  
+    redirect "/contactus/thank-you"
+  end
+  
+  get "/contact/:id/edit/?" do
+    @contact = Contact[params[:id]]
+    erb :"/contact/contact_edit"
+  end
+
+  post "/contact/:id/edit/?" do
+    contact = Contact[params[:id]]
+    contact.update(
+      :name       => params[:name],
+      :company    => params[:company],
+      :email      => params[:email],
+      :phone      => params[:phone],
+      :comment    => params[:comment],
+      :read       => params[:read]
+    )
+
+    redirect "/admin/dashboard"
+  end
+
+  get "/contact/:id/contact/?" do
+    @contact = Contact[params[:id]]
+    erb :"/contact/contact"
+  end
+
+  get "/contact/:id/delete/?" do
+    contact = Contact[params[:id]]
+    contact.destroy
+    redirect "/admin/dashboard"
+  end
+
+end

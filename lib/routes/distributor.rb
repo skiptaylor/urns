d2771ba8920686e6d4922e3f9d4c43997d822cb8
Skip Taylor <skiptaylor@clear.net>
2014-06-23 13:46:41 -0400

    removes logo from dist edit

--- lib/routes/distributor.rb
+++ lib/routes/distributor.rb
@@ -89,8 +89,7 @@ class Routes < Urns::Base
       :facility       => params[:facility],
       :email          => params[:email],
       :username       => params[:username],
-      :password       => params[:password],
-      :logo_id        => params[:logo]
+      :password       => params[:password]
     )
     params[:active] 		? distributor.update(:active => true)    : distributor.update(:active => false)
     params[:allow_po] 	? distributor.update(:allow_po => true)  : distributor.update(:allow_po => false)

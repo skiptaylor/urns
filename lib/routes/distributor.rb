68e023b9cad195532a412627d2855c204aa84198
Skip Taylor <skiptaylor@clear.net>
2014-06-23 13:44:36 -0400

    Makes logo_id => logo

--- lib/routes/distributor.rb
+++ lib/routes/distributor.rb
@@ -90,7 +90,7 @@ class Routes < Urns::Base
       :email          => params[:email],
       :username       => params[:username],
       :password       => params[:password],
-      :logo_id        => params[:logo_id]
+      :logo_id        => params[:logo]
     )
     params[:active] 		? distributor.update(:active => true)    : distributor.update(:active => false)
     params[:allow_po] 	? distributor.update(:allow_po => true)  : distributor.update(:allow_po => false)

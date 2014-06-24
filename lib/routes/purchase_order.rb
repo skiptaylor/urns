dc54037982f9be99ad7be26bdc695e958a96e165
Skip Taylor <skiptaylor@clear.net>
2014-06-24 09:29:18 -0400

    Fixes purchase_order routes

--- lib/routes/purchase_order.rb
+++ lib/routes/purchase_order.rb
@@ -1,32 +1,23 @@
 class Routes < Urns::Base
 
   get "/distributor/:id/purchase_order/new/?" do
-    @service = Service.all
-    unless @purchase = Purchase.where(shopping_session: session[:shopping_session]).first
-      @purchase = Purchase.create(shopping_session: session[:shopping_session])
-    end
-    @distributor = Distributor.where(session: session[:distributor])
+    @distributor = Distributor[params[:distributor_id]]
     @purchase_order = PurchaseOrder.new
-    erb :"/checkout/checkout"
+    erb :"/distributor/purchase_order_edit"
   end
 
   post "/distributor/:id/purchase_order/new/?" do
-    distributor = Distributor.where(session: session[:distributor]).first
-    purchase = Purchase.where(shopping_session: session[:shopping_session]).first
+    distributor = Distributor[params[:distributor_id]]
     PurchaseOrder.create(
       :po_number      => params[:po_number],
       :po_date        => params[:po_date],
       :method         => params[:method],
-      :distributor_id => params[:id]
+      :distributor_id => params[:distributor_id]
     )
   
-    redirect "/distributor/#{params[:id]}/purchase_orders"
+    redirect "/distributor/#{params[:id]}/purchase_order"
   end
-
   
-  
-  
-    
   get "/distributor/:id/purchase_orders/?" do
     @distributor = Distributor[session[:distributor]]
     @purchase_order = PurchaseOrder.all
@@ -43,16 +34,17 @@ class Routes < Urns::Base
   get "/distributor/:distributor_id/purchase_order/:id/edit/?" do
     @distributor = Distributor[params[:distributor_id]]
     @purchase_order = PurchaseOrder[params[:id]]
-    erb :"/distributor/po_edit" 
+    erb :"/distributor/purchase_order_edit" 
   end
 
   post "/distributor/:distributor_id/purchase_order/:id/edit/?" do
     distributor = Distributor[params[:distributor_id]]
     purchase_order = PurchaseOrder[params[:id]]
     purchase_order.update(
-      :po_number  => params[:po_number],
-      :po_date    => params[:po_date],
-      :method     => params[:method]
+      :po_number      => params[:po_number],
+      :po_date        => params[:po_date],
+      :method         => params[:method],
+      :distributor_id => params[:distributor_id]
     )
     
     redirect "/distributor/#{params[:distributor_id]}/purchase_orders"

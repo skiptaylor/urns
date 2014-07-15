1a8fcc4a7c919c06e5307a9210af9fdd67d69ea8
Skip Taylor <skiptaylor@clear.net>
2014-07-15 09:58:00 -0400

    Adds charge.id for Stripe

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -91,7 +91,8 @@ class Routes < Urns::Base
         end
         product.save
       end
-
+      
+      purchase.stripe_id  = charge.id
       purchase.shipping   = purchase.shipping_cost
       purchase.amount     = purchase.grand_total
       purchase.tax        = purchase.tax_rate 

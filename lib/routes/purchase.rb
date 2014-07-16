597fcc63c084f7e437bcec0abac7ea9d1e16da12
Skip Taylor <skiptaylor@clear.net>
2014-07-16 08:07:53 -0400

    fixes tax for purchase save

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -95,7 +95,7 @@ class Routes < Urns::Base
       purchase.stripe_id  = charge.id
       purchase.shipping   = purchase.shipping_cost
       purchase.amount     = purchase.grand_total
-      purchase.tax        = purchase.tax_rate 
+      purchase.tax        = purchase.tax_rate * total
       purchase.save
 
       session[:shopping_session] = nil

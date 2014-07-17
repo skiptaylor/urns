3a1c5c857a72949c5d4a642c2b28c5c9bf8e1b2d
Skip Taylor <skiptaylor@clear.net>
2014-07-17 09:40:23 -0400

    Separates accessories in cart items

--- lib/models/shopping_cart_item.rb
+++ lib/models/shopping_cart_item.rb
@@ -5,10 +5,12 @@ class ShoppingCartItem < Sequel::Model
   
   def self.first_or_increment(shopping_session, product_id)
     shopping_cart_item = nil
-    if item = ShoppingCartItem.first(shopping_session: shopping_session, product_id: product_id)
-       item.update(quantity: item.quantity + 0)
-       shopping_cart_item = item
-    else
+    if item = ShoppingCartItem.first(shopping_session: shopping_session, product_id: product_id, product.accessories == "f")
+      item.update(quantity: item.quantity + 0)
+      shopping_cart_item = item
+    else item = ShoppingCartItem.first(shopping_session: shopping_session, product_id: product_id, product.accessories == "t")
+      shopping_cart_item = item
+    elsif
       shopping_cart_item = ShoppingCartItem.create(shopping_session: shopping_session, product_id: product_id)
     end
     shopping_cart_item

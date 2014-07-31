5dad601825f3a764826eaa9955ea87b4a0bbdcf6
Skip Taylor <skiptaylor@clear.net>
2014-07-31 09:33:57 -0400

    Adds total to tax rate

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -131,7 +131,7 @@ class Routes < Urns::Base
       purchase.stripe_id  = charge.id
       purchase.shipping   = purchase.shipping_cost
       purchase.amount     = purchase.grand_total
-      purchase.tax        = purchase.tax_rate
+      purchase.tax        = purchase.tax_rate * @total
       purchase.save
 
       session[:shopping_session] = nil

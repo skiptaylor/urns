d0abb2fa030081f18168a8983ada9f961e54775d
Skip Taylor <skiptaylor@clear.net>
2014-08-03 09:33:05 -0400

    alters purchase total save -@

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -132,7 +132,7 @@ class Routes < Urns::Base
       
       purchase.stripe_id  = charge.id
       purchase.shipping   = purchase.shipping_cost
-      purchase.total      = @total
+      purchase.total      = total
       purchase.amount     = purchase.grand_total
       purchase.tax        = purchase.tax_rate
       purchase.save

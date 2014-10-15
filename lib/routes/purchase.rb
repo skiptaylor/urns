f12a0cf44c839654caaf2d05ebfb7702bbed6e73
Skip Taylor <skiptaylor@clear.net>
2014-10-15 14:08:07 -0400

    fixes product item

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -131,7 +131,7 @@ class Routes < Urns::Base
         purchase.tax        = purchase.tax_rate
         purchase.save
         
-        Email.receipt(purchase.email, purchase.name, purchase.amount, purchase.referral_code, product.serial_number)
+        Email.receipt(purchase.email, purchase.name, purchase.amount, purchase.referral_code, purchase.cart_items.item.product.serial_number)
 
         session[:shopping_session] = nil
       
@@ -156,7 +156,7 @@ class Routes < Urns::Base
       purchase.tax        = purchase.tax_rate
       purchase.save
       
-      Email.receipt(purchase.email, purchase.name, purchase.amount, purchase.referral_code, product.serial_number)
+      Email.receipt(purchase.email, purchase.name, purchase.amount, purchase.referral_code, purchase.cart_items.item.product.serial_number)
 
       session[:shopping_session] = nil
       

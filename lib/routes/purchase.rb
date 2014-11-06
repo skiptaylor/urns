f5d49b6672355a0debbaa3bbaffbbd28348b9e07
Skip Taylor <skiptaylor@clear.net>
2014-11-06 13:24:31 -0500

    Removes total

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -129,7 +129,6 @@ class Routes < Urns::Base
         purchase.shipping   = purchase.shipping_cost
         purchase.amount     = purchase.grand_total
         purchase.tax        = purchase.tax_rate
-        purchase.total      = total
         purchase.save
         
         @cart = ShoppingCartItem.where(shopping_session: session[:shopping_session])

63a18d866ae7f8e691cbf9cd2024ffa7b351cefa
Skip Taylor <skiptaylor@clear.net>
2014-11-06 12:55:37 -0500

    Tries purchase.total

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -128,7 +128,7 @@ class Routes < Urns::Base
         purchase.stripe_id  = charge.id
         purchase.shipping   = purchase.shipping_cost
         purchase.amount     = purchase.grand_total
-        purchase.tax        = (purchase.tax_rate * total)
+        purchase.tax        = (purchase.tax_rate * purchase.total)
         
         purchase.save
         

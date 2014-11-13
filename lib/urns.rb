a8017db8037dabdd79ab621005f15c29dc4ca80a
Skip Taylor <skiptaylor@clear.net>
2014-11-13 07:46:13 -0500

    removes rack session expire

--- lib/urns.rb
+++ lib/urns.rb
@@ -7,7 +7,6 @@ module Urns
     configure do
       use Rack::Session::Cookie, :key => 'rack.session',
                                  :secret => 'secret123',
-                                 :expire_after => 7200
       set :database,       ENV.fetch('DATABASE_URL')
       set :session_secret, ENV.fetch('SESSION_SECRET')
       set :views,          ['./views', "#{File.dirname(__FILE__)}/views"]

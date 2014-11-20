578d4067b22349326e1da13bd64a0831ccc27a2f
Skip Taylor <skiptaylor@clear.net>
2014-11-20 13:40:30 -0500

    urns.rb

---
+++ lib/urns.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'rack'
+require 'rack/request'
+
+Dotenv.load
+
+module Urns
+  
+  class Base < Sinatra::Application
+    configure do
+      use Rack::SSL
+      use Rack::Protection, :except => :session_hijacking
+      enable :sessions
+      use Rack::Session::Cookie, :key => 'rack.session',
+                                 :secret => 'secret123',
+                                 :expire_after => ENV.fetch("EXPIRE").to_i
+      set :database,       ENV.fetch('DATABASE_URL')
+      set :session_secret, ENV.fetch('SESSION_SECRET')
+      set :views,          ['./views', "#{File.dirname(__FILE__)}/views"]
+    end
+
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
+        aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+            aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+                     provider: ENV.fetch('PROVIDER')
+      }
+      config.fog_directory  = ENV.fetch('FOG_DIRECTORY')
+    end
+
+    require_rel 'models', 'helpers', 'settings'
+    helpers Urns::Helpers
+  end
+
+  class App < Base
+    require_rel 'routes'
+    use Urns::Routes
+  end
+end
+

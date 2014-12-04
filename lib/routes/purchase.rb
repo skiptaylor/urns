f98dc4f0e93efebe0c8b13ace5d67ba96caaff95
Skip Taylor <skiptaylor@clear.net>
2014-12-04 12:06:15 -0500

    removes save sold on

--- lib/routes/purchase.rb
+++ lib/routes/purchase.rb
@@ -126,7 +126,6 @@ class Routes < Urns::Base
           product.save
         end
       
-        purchase.created_at = product.sold_on
         purchase.stripe_id  = charge.id
         purchase.shipping   = purchase.shipping_cost
         purchase.amount     = purchase.grand_total
@@ -157,7 +156,6 @@ class Routes < Urns::Base
         product.save
       end
       
-      purchase.created_at = product.sold_on
       purchase.shipping   = purchase.shipping_cost
       purchase.amount     = purchase.grand_total
       purchase.tax        = purchase.tax_rate
